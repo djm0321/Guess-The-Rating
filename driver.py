ef767644f60e9a727767f66d02e3d054a1f6ff0c
djm0321 <djm0321@live.unc.edu>
2019-10-31 01:25:52 -0400

    Working game in the command line

--- driver.py
+++ driver.py
@@ -1,36 +1,20 @@
 import pandas as pd
 import random
+import gameFunctions
 
 def main():
-    file = "movie_metadata.csv"
-    movies = pd.read_csv(file)
-    rows = movies.shape[0]
-    randMovie = random.randint(0,5042)
-    print(str(movies.values[randMovie][11]) + ", " + str(movies.values[randMovie][25]))
-    movie = movies.values[randMovie][11]
-    movieRating = movies.values[randMovie][25]
-    guess1 = float(input("Guess the rating: "))
-
-    while not isinstance(guess1, float):
-        input("Please input a number u idiot: ")
-
-    guess2 = float(input("Guess the rating: "))
-
-    while not isinstance(guess1, float):
-        input("Please input a number u idiot: ")
-
-    guess1Accuracy = abs(movieRating - guess1)
-    guess1Accuracy = round(guess1Accuracy, 1)
-    print(guess1Accuracy)
-    guess2Accuracy = abs(movieRating - guess2)
-    guess2Accuracy = round(guess2Accuracy, 1)
-    print(guess2Accuracy)
-
-    if guess1Accuracy < guess2Accuracy:
-        print("Player 1 wins! They were " + str(guess1Accuracy) + " points off.")
-    elif guess2Accuracy < guess1Accuracy:
-        print("Player 2 wins! They were " + str(guess2Accuracy) + " points off.")
-    else:
-        print("It's a draw. Y'all are equally bad.")
+    score1 = 0
+    score2 = 0
+    scores = [0, 0]
+    keepPlaying = True
+    movies = gameFunctions.openFile()
+    while keepPlaying:
+        randMovie = gameFunctions.randomMovie(movies)
+        print(str(movies.values[randMovie][11]) + ", " + str(movies.values[randMovie][25]))
+        gameFunctions.guess(movies, randMovie, scores)
+        print(scores)
+        keepPlaying = gameFunctions.playAgain()
+    
+    
      
 main()
---
+++ gameFunctions.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import random
+def openFile():
+    "Opens file. Duh."
+    file = "movie_metadata.csv"
+    movies = pd.read_csv(file)
+    return movies
+
+def randomMovie(movies):
+    "Chooses a random number which coresponds to a random movie from the data set"
+    rows = movies.shape[0]
+    randMovie = random.randint(0,5042)
+    return randMovie
+
+def guess(movies, randMovie, scores):
+    "Runs through one round of guessing for both players."
+    guessed1 = False
+    guessed2 = False
+
+    # Gets Rating of the movie
+    movieRating = movies.values[randMovie][25]
+
+    # Gets guesses from both users and makes sure both values are floats
+    guess1 = input("Guess the rating: ")
+    while not guessed1:
+        try:
+            guess1 = float(guess1)
+        except ValueError:
+            guess1 = input("Please input a number u idiot: ")
+        else:
+            guessed1 = True
+
+    guess2 = input("Guess the rating: ")
+    while not guessed2:
+        try:
+            guess2 = float(guess2)
+        except ValueError:
+            guess2 = input("Please input a number u idiot: ")
+        else:
+            guessed2 = True
+
+    # Checks the accuracy of both guesses and then compares them to each other to determine a winner and gives the winner a point.
+    guess1Accuracy = abs(movieRating - guess1)
+    guess1Accuracy = round(guess1Accuracy, 1)
+    guess2Accuracy = abs(movieRating - guess2)
+    guess2Accuracy = round(guess2Accuracy, 1)
+
+    if guess1Accuracy < guess2Accuracy:
+        print("Player 1 wins! They were " + str(guess1Accuracy) + " points off.")
+        scores[0] += 1
+    elif guess2Accuracy < guess1Accuracy:
+        print("Player 2 wins! They were " + str(guess2Accuracy) + " points off.")
+        scores[1] += 1
+    else:
+        print("It's a draw. Y'all are equally bad.")
+
+def playAgain():
+    userInput = input("Do you want to play again? Type 'no' if not. If you don't type 'no', you gotta play again anyways u scrub")
+    if userInput == "no":
+        return False
+    else:
+        return True
+
+    
