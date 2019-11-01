fc58d0d752f1f1f27ab677f81a4acd366b7b43f5
Abby Geigerman <alg21@rice.edu>
2019-11-01 01:00:08 -0500

    Flask structure filled in

--- flask/main.py
+++ flask/main.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, World!"
+def start():
+    return render_template("start.html")
     
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/play")
+def play():
+    return render_template("play.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
